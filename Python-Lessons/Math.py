539a0bed5477255e9e0cc5c3fa1b6f8c7e11135e
Mehmet ERÖZKAN <92365223+mehmeterozkan91@users.noreply.github.com>
2024-06-02 18:33:34 +0300

    first commit

---
+++ Python-Lessons/Math.py
@@ -0,0 +1,20 @@
+a = 4
+b = 3
+
+print("Toplama: ", a + b)
+print("Tam sayı bölmesi: ", a // b)
+print("Çıkarma: ", a - b)
+print("Çarpma: ", a * b)
+print("Bölme: ", a / b)
+print("Mod alma (Kalanı bulma): ", a % b)
+print("Üssünü bul: ", a ** b)
+
+print("Kare kök bulma: ", a ** 0.5)
+print("Küp kök bulma: ", 8 ** (1 / 3))
+
+# Operatörleri beraber kullanma
+# Öncelik sıralaması: Parantez, çarpma, bölme, toplama, çıkarma
+# İşlemler soldan sağa değerlendirilir.
+
+print("Sonuç: ", 8 + 4 * 4 / 2 - 18)
+print("Sonuç: ", ((8 + 4) * (4 / 2)) - 18)
